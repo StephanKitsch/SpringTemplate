4385a420b07250feb0727dae25ab7e2231934297
Stephan Kitschminyof <stephan.kitsch.pro@gmail.com>
2023-04-01 16:06:16 +0200

    :sparkles: hello world controller

--- src/main/java/stephan/kitschminyof/springtemplate/controller/HelloWorldController.java
+++ src/main/java/stephan/kitschminyof/springtemplate/controller/HelloWorldController.java
@@ -1,2 +1,16 @@
-package stephan.kitschminyof.springtemplate.controller;public class HelloWorldController {
+package stephan.kitschminyof.springtemplate.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/api")
+    public ResponseEntity<String> helloWorld() {
+        return new ResponseEntity<>("Hello world !", HttpStatus.OK);
+    }
 }
