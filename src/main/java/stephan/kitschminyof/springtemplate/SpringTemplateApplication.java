4f97f4310359595d72a9c4c1f1365e5541241e83
Stephan Kitschminyof <stephan.kitsch.pro@gmail.com>
2023-04-01 16:06:01 +0200

    :tada: init spring boot project

--- src/main/java/stephan/kitschminyof/springtemplate/SpringTemplateApplication.java
+++ src/main/java/stephan/kitschminyof/springtemplate/SpringTemplateApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringTemplateApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringTemplateApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTemplateApplication.class, args);
+    }
 
 }
--- src/test/java/stephan/kitschminyof/springtemplate/SpringTemplateApplicationTests.java
+++ src/test/java/stephan/kitschminyof/springtemplate/SpringTemplateApplicationTests.java
@@ -2,12 +2,13 @@ package stephan.kitschminyof.springtemplate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class SpringTemplateApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
